446984aa6da59e753b6d09d230228121d60a2580
Aleksandr <sky7way@yandex.ru>
2023-05-14 22:55:55 +0300

    getIndexToIns

--- getIndexToIns.js
+++ getIndexToIns.js
@@ -1,19 +1,28 @@
+// function getIndexToIns(arr, num) {
+//     arr.sort((a, b) => {
+//      return a - b;
+//     })
+//     if (arr.length === 0){
+//       return 0; 
+//     } else if(num >= arr[arr.length - 1]){
+//         return arr.length;
+//       }
+//     else{
+//     for(let i = 0; i < arr.length; i++){
+//       if(num <= arr[i]){
+//         return i;
+//     }
+//   }
+//  }
+// }
+
+////////////////////////////////
+
 function getIndexToIns(arr, num) {
-    arr.sort((a, b) => {
-     return a - b;
-    })
-    if (arr.length === 0){
-      return 0; 
-    } else if(num >= arr[arr.length - 1]){
-        return arr.length;
-      }
-    else{
-    for(let i = 0; i < arr.length; i++){
-      if(num <= arr[i]){
-        return i;
-    }
+    return arr
+      .concat(num)
+      .sort((a, b) => a - b)
+      .indexOf(num);
   }
- }
-}
   
   getIndexToIns([2, 5, 10], 15);
