3c2c5c9748a32248d198c98143d0fdfc5785afe5
Aleksandr <sky7way@yandex.ru>
2023-05-14 18:28:57 +0300

    titleCase v2

--- titleCase.js
+++ titleCase.js
@@ -1,8 +1,20 @@
-function titleCase(str) {
-    const arr = str.toLowerCase()
-                .split(' ')
-                .map(i => i.charAt(0).toUpperCase() + i.slice(1));
-    return arr.join(' ');
-  }
+// function titleCase(str) {
+//     const arr = str.toLowerCase()
+//                 .split(' ')
+//                 .map(i => i.charAt(0).toUpperCase() + i.slice(1));
+//     return arr.join(' ');
+//   }
   
   titleCase("sHoRt AnD sToUt");
+
+  ////////////////////////////////
+
+  function titleCase(str) {
+    return str
+      .toLowerCase()
+      .split(" ")
+      .map(val => val.replace(val.charAt(0), val.charAt(0).toUpperCase()))
+      .join(" ");
+  }
+  
+  titleCase("I'm a little tea pot");
