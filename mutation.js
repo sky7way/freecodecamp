6630fd6c02434bc5cdfc8b4cf0ff53961f304d1f
Aleksandr <sky7way@yandex.ru>
2023-05-15 09:02:10 +0300

    mutation

--- mutation.js
+++ mutation.js
@@ -1,11 +1,20 @@
+// function mutation(arr) {
+//     const arr1 = arr[0].toLowerCase();
+//     const arr2 = arr[1].toLowerCase();
+//     for(let i = 0; i < arr2.length; i++){
+//       if(arr1.indexOf(arr2[i]) < 0){
+//         return false;
+//       }
+//     }return true;
+//   }
+
+//////////////////////////
+
 function mutation(arr) {
-    const arr1 = arr[0].toLowerCase();
-    const arr2 = arr[1].toLowerCase();
-    for(let i = 0; i < arr2.length; i++){
-      if(arr1.indexOf(arr2[i]) < 0){
-        return false;
-      }
-    }return true;
+    return arr[1].toLowerCase().split('').every(function(e){
+      return arr[0].toLowerCase().indexOf(e) !== -1;
+    });
   }
+  
   
 mutation(["hello", "hey"]);
